e127a7772a329263ded898f8fb47637dc5e471f8
SRIDHARANITHARAN.B <username420sri@gmail.com>
2024-08-02 05:38:43 -0700

    Create set_nth_bit.c

---
+++ BITWISE_OPERATOR/set_nth_bit.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int num,n, bitset;
+    printf("Enter you number :");
+    scanf("%d",&num);
+    printf("Enter set bit index of the number :0-31 :");
+    scanf("%d",&n);
+    bitset = (1<<n) | num;
+   printf("%d",bitset);
+
+}
